4ba935bfb66233d0276d9e5fd8f67f6f425c7644
Rijusmit Biswas <talk.riju@gmail.com>
2022-09-07 23:58:38 +0530

    git new

---
+++ hellocpp/y2022/array.cpp
@@ -0,0 +1,28 @@
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+   string word;
+   int a;
+   cout << "Enter a word\n";
+   cin >> word;
+   a = word.length();
+   int i=0;
+
+   string wordrl[a]; 
+   while(i<a){
+       
+       wordrl[i]=word[a-(i+1)];
+       i++;
+   }
+    string wordr="";
+    int x;
+    for(x=0;x<a;x++){
+        wordr=wordr + wordrl[x];
+    }
+    cout << "reverse word is\n" << wordr;
+    return 0;
+}
