bd90dbf7203f387a224ca521e3b1e6bdf08c3306
Rijusmit Biswas <talk.riju@gmail.com>
2022-09-08 21:54:37 +0530

    commit

--- helloc/@project4/pointer4.c
+++ helloc/@project4/pointer4.c
@@ -15,7 +15,7 @@ int main(){
 
     for(int i=0;i<10;i++){
         for(int j=0; j<10; j++){
-            printf("%5d",mat[i][j]);
+            printf("%5d",*(*(mat+i)+j));
         }
         printf("\n");
     }
